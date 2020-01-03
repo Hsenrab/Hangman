b00fb497db8517d081db8453bcb0c0c05c453c9b
HSenrab <hllewoh@gmail.com>
2020-01-03 14:32:33 +0000

    #6 stopped player from guessing the same letter more than once

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ class HangmanWord:
     def __init__(self, word):
         self.hidden_word = list(word)
         self.player_word = ["_" for char in self.hidden_word]
+        self.letters_tried = []
 
     def create_printable_player_word(self):
         print_string = ""
@@ -21,6 +22,7 @@ class HangmanWord:
         return print_string
 
     def is_letter_correct(self, letter):
+        self.letters_tried.append(letter)
         letter_found = False
 
         for index, char in enumerate(self.hidden_word):
@@ -29,5 +31,8 @@ class HangmanWord:
                 self.player_word[index] = letter
 
         return letter_found
+
+    def has_letter_been_tried(self, letter):
+        return letter in self.letters_tried
 
 
