868f090b7dc16977b68eeddc08bd2686d032aa78
HSenrab <hllewoh@gmail.com>
2020-01-03 14:17:21 +0000

    #4 Now asks player for single letter

--- interaction.py
+++ interaction.py
@@ -8,5 +8,24 @@ def welcome():
     print("Let's begin:")
 
 
-def display_length(word):
-    print("The word I am thinking of has", len(word), "letters.")
+def display_length(hangman_word):
+    print("The word I am thinking of has", len(hangman_word.hidden_word), "letters.")
+
+
+def display_player_word(hangman_word):
+    print("Current state: ", hangman_word.create_printable_player_word())
+
+
+def ask_player_for_letter():
+    print("Please choose a letter")
+    letter = input()
+    return letter
+
+
+def letter_correct(letter):
+    print("Yes!,", letter, "is in my word")
+
+
+def letter_incorrect(letter):
+    print("Sorry", letter, "is not in my word")
+
